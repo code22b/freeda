b4acbf8286b8e5287da46a47f16fdc667460a5f5
code22b <pasalicbarisa@gmail.com>
2023-12-08 17:30:03 +0100

    Zavrsena sekcija meni

--- parsals/meni part.php
+++ parsals/meni part.php
@@ -4,70 +4,65 @@
         <div class="row">
             <div class="wrapper">
                 <div class="menu-item">
-                   <div class="img">
-                   <img src="img/coffee3.png" alt="">
-                   </div>
-                   <div class="text">
-                    <h2>Kafa i krofna</h2>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <span>200 din</span>
-                   </div>
+                    <div class="img">
+                        <img src="img/coffee3.png" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>KAFA I KROFNA</h3>
+                        <p>Ut placerat lacinia commodo.</p>
+                        <span>200 din</span>
+                    </div>
                 </div>
                 <div class="menu-item">
-                   <div class="img">
-                   <img src="img/coffee3.png" alt="">
-                   </div>
-                   <div class="text">
-                    <h2>Kafa i krofna</h2>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <span>200 din</span>
-                   </div>
+                    <div class="img">
+                        <img src="img/coffee7.png" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>KAFA I KROASAN</h3>
+                        <p>Ut placerat lacinia commodo.</p>
+                        <span>200 din</span>
+                    </div>
                 </div>
                 <div class="menu-item">
-                   <div class="img">
-                   <img src="img/coffee3.png" alt="">
-                   </div>
-                   <div class="text">
-                    <h2>Kafa i krofna</h2>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <span>200 din</span>
-                   </div>
+                    <div class="img">
+                        <img src="img/coffee8.png" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>PROJA</h3>
+                        <p>Ut placerat lacinia commodo.</p>
+                        <span>200 din</span>
+                    </div>
                 </div>
                 <div class="menu-item">
-                   <div class="img">
-                   <img src="img/coffee3.png" alt="">
-                   </div>
-                   <div class="text">
-                    <h2>Kafa i krofna</h2>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <span>200 din</span>
-                   </div>
+                    <div class="img">
+                        <img src="img/coffee9.png" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>JAJA I ŠUNKARICA</h3>
+                        <p>Ut placerat lacinia commodo.</p>
+                        <span>200 din</span>
+                    </div>
                 </div>
-                
                 <div class="menu-item">
-                   <div class="img">
-                   <img src="img/coffee3.png" alt="">
-                   </div>
-                   <div class="text">
-                    <h2>Kafa i krofna</h2>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <span>200 din</span>
-                   </div>
+                    <div class="img">
+                        <img src="img/coffee10.png" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>SMOOTHIEA</h3>
+                        <p>Ut placerat lacinia commodo.</p>
+                        <span>200 din</span>
+                    </div>
                 </div>
-                
                 <div class="menu-item">
-                   <div class="img">
-                   <img src="img/coffee3.png" alt="">
-                   </div>
-                   <div class="text">
-                    <h2>Kafa i krofna</h2>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <span>200 din</span>
-                   </div>
+                    <div class="img">
+                        <img src="img/coffee11.png" alt="">
+                    </div>
+                    <div class="text">
+                        <h3>SENDVIČ</h3>
+                        <p>Ut placerat lacinia commodo.</p>
+                        <span>200 din</span>
+                    </div>
                 </div>
-                
-                
-
             </div>
         </div>
     </div>
