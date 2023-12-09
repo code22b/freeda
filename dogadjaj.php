68a700a400ad34cd9c9b658edfbb4749b34e3a77
code22b <pasalicbarisa@gmail.com>
2023-12-09 22:06:21 +0100

    Gotov projekat verzija 1

---
+++ dogadjaj.php
@@ -0,0 +1,11 @@
+<?php include 'parsals/header.php' ?>
+
+<!-- header -->
+<header class="headers">
+    <img src="img/coffee-6.png" alt="">
+    <h1>Dogadjaji</h1>
+</header>
+
+<?php include 'parsals/dogadjaji-part.php' ?>
+
+<?php include 'parsals/footer.php' ?>
---
+++ usluge.php
@@ -0,0 +1,11 @@
+<?php include 'parsals/header.php' ?>
+
+<!-- header -->
+<header class="headers">
+    <img src="img/coffee-6.png" alt="">
+    <h1>Usluge</h1>
+</header>
+
+<?php include 'parsals/usluge part.php' ?>
+
+<?php include 'parsals/footer.php' ?>
